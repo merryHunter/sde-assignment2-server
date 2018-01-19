ce355885836a3db0600c880bc8cc415ab67b8e66
merryHunter <chernuhaiv@gmail.com>
2018-01-19 18:01:01 +0100

    finished backend resouce Person

--- src/rest/activity/resource/PersonResource.java
+++ src/rest/activity/resource/PersonResource.java
@@ -9,11 +9,14 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
@@ -134,9 +137,87 @@ public class PersonResource {
     }
     
     
+    // Request #8
+    @GET
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    @Path("{activity_type}/{activity_id}")
+    public List<Activity> getActivityByTypeId(@PathParam("activity_type") String type,
+    										@PathParam("activity_id") int activity_id){
+    	Person p = getPersonById(id);
+    	List<Activity> acts = new ArrayList<Activity>();
+    	for (Activity a: p.getActivityPreferences()) {
+    		if (a.getType().equals(type) && a.getIdActivity() == activity_id){
+    			acts.add(a);
+    		}
+    	}
+		return acts;
+    }
     
+    //Request #9
+    @POST
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.TEXT_XML })
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    @Path("{activity_type}")
+    public List<Activity> addActivity(@PathParam("activity_type") String type, Activity activity){
+    	Person p = getPersonById(id);
+    	List<Activity> acts = p.getActivityPreferences();
+    	logger.info("adding new activity..");
+    	activity.setType(type);
+    	Activity.saveActivity(activity);
+    	acts.add(activity);
+    	p.setActivityPreferences(acts);
+    	Person.updatePerson(p);
+		return acts;
+    }
     
+    // Request #10
+    @PUT
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    @Path("{activity_type}/{activity_id}")
+    public Response updateActivityType(@PathParam("activity_type") String activity_type, 
+								@PathParam("activity_id") int activity_id, Activity activity) {
+        logger.info("--> Updating Activity... " +this.id);
+        Response res;
+        Person existing = getPersonById(this.id);
+
+        if (existing == null) {
+            res = Response.noContent().build();
+        } else {
+        	boolean activity_exist = false;
+        	//get activities of existing person
+        	List<Activity> acts = existing.getActivityPreferences();
+        	for (int i = 0; i < acts.size(); i++) {
+        		Activity a = acts.get(i);
+        		if (a.getIdActivity() == activity_id) {
+        			activity_exist = true;
+        			a.setDescription(activity.getDescription());
+        			a.setName(activity.getName());
+        			a.setPlace(activity.getPlace());
+        			a.setStartdate(activity.getStartdate());
+        			a.setType(activity.getType()); 	
+        			a.setPerson(existing);
+        		}
+        		Activity.updateActivity(a);
+        		Person.updatePerson(existing);
+        	}
+        	if ( !activity_exist) {
+        		throw new RuntimeException("Update: Activity with id " + activity_id
+                        + " not found");
+        	}
+            res = Response.ok(uriInfo.getAbsolutePath()).build();
+        }
+        return res;
+    }
     
-    
-    
+    // Request #11
+    // Not implemented
+    @GET
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    @Path("{activity_type}?before={beforeDate}&after={afterDate} ")
+    public List<Activity> getActivityWithinDates(@PathParam("activity_Type") String type, 
+			@DefaultValue("")@QueryParam("before") String beforeDate, 
+			@DefaultValue("")@QueryParam("after") String afterDate) {
+    	throw new RuntimeException("Not implemented!");
+    	
+    }
 }
